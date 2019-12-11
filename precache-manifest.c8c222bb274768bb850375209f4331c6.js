3c8b23a9df9d2ae8c67b0823dfe24c5e9d7fce47
bawierino <x.hofmann91@gmail.com>
2019-12-11 22:00:35 +0100

    Updates

---
+++ precache-manifest.c8c222bb274768bb850375209f4331c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0be11acc7a0630cf670ff937d73febd",
+    "url": "/tradez/index.html"
+  },
+  {
+    "revision": "17b85a6b7ffce2d19ce5",
+    "url": "/tradez/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1bf503465bd99315df3e",
+    "url": "/tradez/static/js/2.a95cb36e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tradez/static/js/2.a95cb36e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "17b85a6b7ffce2d19ce5",
+    "url": "/tradez/static/js/main.5e3e0ef6.chunk.js"
+  },
+  {
+    "revision": "00440189fba5aa78bf34",
+    "url": "/tradez/static/js/runtime-main.7f7b79c2.js"
+  }
+]);
