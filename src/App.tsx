72653b01d3ca0c590d801e8e2bb013b912b82c84
bawierino <x.hofmann91@gmail.com>
2019-12-12 20:26:39 +0100

    show some cards that we need

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import React from "react";
-import { allCards } from "./data/all_cards";
+import { allFFDecksCards } from "./data/all_ffdecks_cards";
+import { mapFFDecksCards } from "./data/transformations/map_ff_decks_cards";
+import { Rarity } from "./data/structures/rarity";
+import { getTotalQuantity } from "./data/transformations/get_total_quanity";
 
 const App: React.FC = () => {
-    return <div>{JSON.stringify(allCards)}</div>;
+    return (
+        <div>
+            {mapFFDecksCards(allFFDecksCards)
+                .filter(
+                    c =>
+                        c.rarity === Rarity.LEGEND &&
+                        getTotalQuantity(c) < c.minimalWantedQuantity &&
+                        getTotalQuantity(c) > 0
+                )
+                .map(c => (
+                    <div key={c.serial}>
+                        {c.name} {c.serial} we want at least {c.minimalWantedQuantity} but we have only{" "}
+                        {getTotalQuantity(c)}
+                    </div>
+                ))}
+        </div>
+    );
 };
 
 export default App;
