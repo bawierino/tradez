c2406671e0f3f912f6b31acb80e8d85620bfa484
bawierino <x.hofmann91@gmail.com>
2019-12-16 00:35:57 +0100

    use filter bar in wanted cards page, too

--- src/components/pages/wanted_cards_page/wanted_cards_page_component.tsx
+++ src/components/pages/wanted_cards_page/wanted_cards_page_component.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { FFCard } from "../../../data/structures/ff_card";
 import { WantedCardComponent } from "../../wanted_card/wanted_card_component";
 import { PageProps } from "../page_props";
+import { FilterBarComponent } from "../../filter_bar/filter_bar_component";
 
 export interface WantedCardsPageProps extends PageProps {
     cards: FFCard[];
@@ -9,9 +10,16 @@ export interface WantedCardsPageProps extends PageProps {
 
 export const WantedCardsPageComponent: React.FC<WantedCardsPageProps> = (props: WantedCardsPageProps) => {
     const { cards } = props;
+    const [filteredCards, setFilteredCards] = React.useState(cards);
+
     return (
         <div>
-            {cards.map(c => (
+            <FilterBarComponent
+                cards={cards}
+                onFilter={cards => setFilteredCards(cards)}
+                showTradeableVersionFilter={false}
+            />
+            {filteredCards.map(c => (
                 <WantedCardComponent card={c} key={c.serial} />
             ))}
         </div>
