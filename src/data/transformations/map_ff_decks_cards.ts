102626a13322d249f568e7ef012835670737ee17
bawierino <x.hofmann91@gmail.com>
2019-12-16 20:16:54 +0100

    allow overwriting properties assigned to cards by card serial

--- src/data/transformations/map_ff_decks_cards.ts
+++ src/data/transformations/map_ff_decks_cards.ts
@@ -5,18 +5,54 @@ import { FFCard } from "../structures/ff_card";
 
 export function mapFFDecksCards(ffDecksCards: FFDecksCard[]): FFCard[] {
     return ffDecksCards.map(ffCard => {
+        const serial = ffCard["Serial number"];
+
+        const specificCardProperties = getSpecificCardProperties(serial);
+
         return {
             name: ffCard.Name,
-            normal: { quantity: ffCard["regular quantity"], minimalQuantityWanted: 0 },
-            foil: { quantity: ffCard["foil quantity"], minimalQuantityWanted: 1 },
-            fullArt: { quantity: 0, minimalQuantityWanted: 0 },
-            foilArt: { quantity: 0, minimalQuantityWanted: 0 },
-            alternateArt: { quantity: 0, minimalQuantityWanted: 0 },
-            alternateArtFoil: { quantity: 0, minimalQuantityWanted: 0 },
+            normal: {
+                quantity: ffCard["regular quantity"],
+                minimalQuantityWanted: 0,
+                ...specificCardProperties.normal
+            },
+            foil: {
+                quantity: ffCard["foil quantity"],
+                minimalQuantityWanted: 1,
+                ...specificCardProperties.foil
+            },
+            fullArt: { quantity: 0, minimalQuantityWanted: 0, ...specificCardProperties.fullArt },
+            foilArt: { quantity: 0, minimalQuantityWanted: 0, ...specificCardProperties.foilArt },
+            alternateArt: { quantity: 0, minimalQuantityWanted: 0, ...specificCardProperties.alternateArt },
+            alternateArtFoil: {
+                quantity: 0,
+                minimalQuantityWanted: 0,
+                ...specificCardProperties.alternateArt
+            },
             rarity: getRarityByFFRarity(ffCard.rarity),
-            serial: ffCard["Serial number"],
-            opus: getOpusBySerial(ffCard["Serial number"]),
-            minimalWantedQuantity: 3
+            serial,
+            opus: getOpusBySerial(serial),
+            minimalWantedQuantity: specificCardProperties.minimalWantedQuantity || 3
         };
     });
+}
+
+type RecursivePartial<T> = {
+    [P in keyof T]?: T[P] extends (infer U)[]
+        ? RecursivePartial<U>[]
+        : T[P] extends object | undefined
+        ? RecursivePartial<T[P]>
+        : T[P];
+};
+
+function getSpecificCardProperties(
+    serial: string
+): Omit<RecursivePartial<FFCard>, "name" | "serial" | "rarity" | "opus"> {
+    switch (serial) {
+        case "3-056":
+            return { minimalWantedQuantity: 6 };
+        case "10-068":
+            return { foilArt: { quantity: 2, minimalQuantityWanted: 1 } };
+    }
+    return {};
 }
