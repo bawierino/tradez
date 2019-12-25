93dbbd260772dd20084e7d31f0e1356bf9c4a82a
bawierino <x.hofmann91@gmail.com>
2019-12-25 18:59:13 +0100

    remove all css files

--- src/App.tsx
+++ src/App.tsx
@@ -16,10 +16,20 @@ const App: React.FC = () => {
     const htmlRef = React.useRef(document.documentElement);
     const scrollShadowClassName = useScrollShadowClassName(htmlRef);
     htmlRef.current.className = scrollShadowClassName;
-
     document.body.className = css`
+        label: global-style;
+        margin: 0;
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell",
+            "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
         background: linear-gradient(to right, ${colors.primary.hoveryLight}42, transparent);
         -webkit-tap-highlight-color: transparent;
+        *,
+        ::before,
+        ::after {
+            box-sizing: border-box;
+        }
     `;
 
     switch (routerAPI.getCurrentPageId()) {
