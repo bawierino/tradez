2c50fbac794344acdc2eae6a913405f69b952d62
bawierino <x.hofmann91@gmail.com>
2019-12-25 19:30:26 +0100

    make popover shadow color light

--- src/design_system/constants/box_shadows.ts
+++ src/design_system/constants/box_shadows.ts
@@ -3,7 +3,7 @@ import { colors } from "./colors";
 export const boxShadows = {
     default: `0px 2px 6px 0px ${colors.shadows.default}`,
     high: `0px 2px 8px 0px ${colors.shadows.default}`,
-    popover: `0px 4px 24px 0px ${colors.shadows.default}`,
+    popover: `0px 4px 24px 0px ${colors.shadows.light}`,
     close: `0 0 2px 0 ${colors.shadows.dark}`,
     deep: `0 1px 4px ${colors.shadows.default} inset`,
     deeper: `0 2px 6px ${colors.shadows.dark} inset`
